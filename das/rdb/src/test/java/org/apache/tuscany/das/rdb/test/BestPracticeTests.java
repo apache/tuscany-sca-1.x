50b9c0c623581265c105fab1b4c0967377e90d05
Kevin Joe Williams <kwilliams@apache.org>
2006-02-21 03:20:14 +0000

    Beginning of best practice tests.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java@379318 13f79535-47bb-0310-9956-ffa450edef68

---
+++ das/rdb/src/test/java/org/apache/tuscany/das/rdb/test/BestPracticeTests.java
@@ -0,0 +1,44 @@
+/**
+ *
+ *  Copyright 2005 The Apache Software Foundation or its licensors, as applicable.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.tuscany.das.rdb.test;
+
+import org.apache.tuscany.das.rdb.Command;
+import org.apache.tuscany.das.rdb.CommandGroup;
+import org.apache.tuscany.das.rdb.test.data.CompanyData;
+import org.apache.tuscany.das.rdb.test.framework.DasTest;
+
+import commonj.sdo.DataObject;
+
+public class BestPracticeTests extends DasTest {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        new CompanyData(getAutoConnection()).refresh();
+    }
+
+    //Added this to test code used in the Stand alone companies test.  This is not working
+    //because datasource is specified in the config
+    public void testReadCompanies() throws Exception {
+
+        CommandGroup commandGroup = CommandGroup.FACTORY.createCommandGroup(getConfig("CompanyConfig.xml"));
+        Command read = commandGroup.getCommand("all companies");
+        DataObject root = read.executeQuery();
+        root.getList("COMPANY");
+
+    }
+
+}
