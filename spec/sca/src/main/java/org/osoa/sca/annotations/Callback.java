3dd99d15050a27e9b3f2517868777826e5532aef
Jeremy Boynes <jboynes@apache.org>
2006-03-10 04:08:30 +0000

    cosmetic - remove import * that crept in with bad ide settings
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java@384698 13f79535-47bb-0310-9956-ffa450edef68

--- spec/sca/src/main/java/org/osoa/sca/annotations/Callback.java
+++ spec/sca/src/main/java/org/osoa/sca/annotations/Callback.java
@@ -16,7 +16,9 @@
  */
 package org.osoa.sca.annotations;
 
-import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
 import java.lang.annotation.Retention;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Target;
