e479d598f21433dab579c215c5c87be7b3c70a1a
Anthony Elder <antelder@apache.org>
2006-03-27 09:30:34 +0000

    Start work on javaScript samples
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java@389082 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/JavaScript/sample2/src/test/java/sample/Sample2TestCase.java
@@ -0,0 +1,38 @@
+/**
+ *
+ *  Copyright 2005 The Apache Software Foundation or its licensors, as applicable.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package sample;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+public class Sample2TestCase extends TestCase {
+
+    public void testGeetings() throws Exception {
+// TODO: default properties don't work   
+//        String in = "Petra";
+//        String out = Sample2Client.invoke(in);
+//        assertEquals("Hi Petra", out);
+    }
+
+    public void testDeutschGeetings() throws Exception {
+        String in = "Sue";
+        String out = Sample2Client.invokeDeutsch(in);
+        assertEquals("Guten Tag Sue", out);
+    }
+}
---
+++ samples/JavaScript/sample5/src/main/java/sample/utils/Salutation.java
@@ -0,0 +1,14 @@
+package sample.utils;
+
+public class Salutation {
+    
+    private String salutation = "Kia ora";
+    
+    public String getSalutation() {
+        return salutation;
+    }
+
+    public void getSalutation(String s) {
+        salutation = s;
+    }
+}
