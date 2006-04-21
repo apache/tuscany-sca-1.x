395007acec0b05b65b9c166fc512c862e6d90f91
Anthony Elder <antelder@apache.org>
2006-04-21 11:45:52 +0000

    Start tidying up the jsonrpc binding
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java@395862 13f79535-47bb-0310-9956-ffa450edef68

--- samples/helloworld/helloworldjsonrpc/src/main/webapp/helloworld.js
+++
@@ -1,6 +0,0 @@
-
-function getGreeting() {
-    var name = document.getElementById("name").value;
-    var result = SCA.HelloWorldService.getGreetings(name);
-    document.getElementById("greeting").value = result;
-}
