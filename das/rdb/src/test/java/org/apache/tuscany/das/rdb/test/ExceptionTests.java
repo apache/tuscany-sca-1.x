2f8999979e3ea887a2022990fb67b4a323bf983a
Kevin Joe Williams <kwilliams@apache.org>
2006-04-28 02:25:56 +0000

    Adding test for "Throw meaningful exception if users passes a null config InputStream"
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java@397740 13f79535-47bb-0310-9956-ffa450edef68

--- das/rdb/src/test/java/org/apache/tuscany/das/rdb/test/ExceptionTests.java
+++ das/rdb/src/test/java/org/apache/tuscany/das/rdb/test/ExceptionTests.java
@@ -17,6 +17,7 @@
 package org.apache.tuscany.das.rdb.test;
 
 import org.apache.tuscany.das.rdb.Command;
+import org.apache.tuscany.das.rdb.CommandGroup;
 import org.apache.tuscany.das.rdb.test.company.CompanyFactory;
 import org.apache.tuscany.das.rdb.test.customer.DataGraphRoot;
 import org.apache.tuscany.das.rdb.test.data.CustomerData;
@@ -28,48 +29,73 @@ import commonj.sdo.helper.TypeHelper;
 
 public class ExceptionTests extends DasTest {
 
-	public ExceptionTests() {
-		super();
-	}
+    public ExceptionTests() {
+        super();
+    }
 
-	protected void setUp() throws Exception {
-		super.setUp();
-		
-		new CustomerData(getAutoConnection()).refresh();
-		new OrderData(getAutoConnection()).refresh();
-		
-	}
+    protected void setUp() throws Exception {
+        super.setUp();
 
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
+        new CustomerData(getAutoConnection()).refresh();
+        new OrderData(getAutoConnection()).refresh();
 
-	public void testMissingConnection() throws Exception {
-		Command readCustomers = Command.FACTORY
-		.createCommand("select * from CUSTOMER where ID = 1");
-		try {
-			readCustomers.executeQuery();
-			fail("RuntimeException should be thrown");
-		} catch ( RuntimeException ex) {
-			assertEquals("A DASConnection object must be specified before executing the query.", ex.getMessage());
-		}
-		
-	}
-	public void testMissingMapping() throws Exception {
-		Command readCustomers = Command.FACTORY
-				.createCommand("select * from CUSTOMER where ID = 1");
-		readCustomers.setConnection(getConnection());
-		SDOUtil.registerStaticTypes(CompanyFactory.class);
-		readCustomers.setDataObjectModel(TypeHelper.INSTANCE.getType(DataGraphRoot.class));
+    }
 
-		try {
-			readCustomers.executeQuery();
-			fail("Exception should be thrown");
-		} catch (RuntimeException ex) {	
-			assertEquals("An SDO Type with name CUSTOMER was not found", ex
-					.getMessage());
-		}
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 
-	}
+    public void testMissingConnection() throws Exception {
+        Command readCustomers = Command.FACTORY.createCommand("select * from CUSTOMER where ID = 1");
+        try {
+            readCustomers.executeQuery();
+            fail("RuntimeException should be thrown");
+        } catch (RuntimeException ex) {
+            assertEquals("A DASConnection object must be specified before executing the query.", ex.getMessage());
+        }
 
+    }
+
+    public void testMissingMapping() throws Exception {
+        Command readCustomers = Command.FACTORY.createCommand("select * from CUSTOMER where ID = 1");
+        readCustomers.setConnection(getConnection());
+        SDOUtil.registerStaticTypes(CompanyFactory.class);
+        readCustomers.setDataObjectModel(TypeHelper.INSTANCE.getType(DataGraphRoot.class));
+
+        try {
+            readCustomers.executeQuery();
+            fail("Exception should be thrown");
+        } catch (RuntimeException ex) {
+            assertEquals("An SDO Type with name CUSTOMER was not found", ex.getMessage());
+        }
+
+    }
+
+    /**
+     * Test ability to describe problem when a user passes an empty stream
+     */
+    public void testEmptyStream() throws Exception {
+        try {
+            Command.FACTORY.createCommand("select * from CUSTOMER where ID = 1", getConfig("NonExistingFile.xml"));
+            fail("Error should be thrown");
+        } catch (Error e) {
+            assertEquals(
+                    "Cannot load configuration from a null InputStream. Possibly caused by an incorrect config xml file name",
+                    e.getMessage());
+        }
+    }
+
+    /**
+     * Test ability to describe problem when a user passes an empty stream
+     */
+    public void testEmptyStream2() throws Exception {
+        try {
+            CommandGroup.FACTORY.createCommandGroup(getConfig("NonExistingFile.xml"));
+            fail("Error should be thrown");
+        } catch (Error e) {
+            assertEquals(
+                    "Cannot load configuration from a null InputStream. Possibly caused by an incorrect config xml file name",
+                    e.getMessage());
+        }
+    }
 }
