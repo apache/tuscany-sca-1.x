5584e8331c255fdfda24fe8a4d81a88f6346cf3c
Jean Sebastien Delfino <jsdelfino@apache.org>
2006-05-04 07:14:28 +0000

    Fix for TUSCANY-268 - starting to refactor the samples
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java@399576 13f79535-47bb-0310-9956-ffa450edef68

--- samples/helloworld/helloworldaxis/axiscmds.bat
+++
@@ -1,2 +0,0 @@
-wsdl2java -s -S true -o wsdl2javaOut helloworld.wsdl
-java2wsdl -o helloworld.wsdl -l http://localhost:8080/axis/services/helloworld -y WRAPPED org.apache.tuscany.samples.helloworldaxis.HelloWorldServiceImpl
