4a7048924c32f56989748e5b672a18ef5a181cbf
Charles Rick Rineholt <rineholt@apache.org>
2006-05-13 23:00:49 +0000

    doing it twice ?
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java@406159 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/build-dist.bat
+++ distribution/build-dist.bat
@@ -29,34 +29,3 @@ echo Creating Tuscany distribution ...
 call mvn clean install
 cd /d "%CUR_DIR%"
 @endlocal
-@REM ----------------------------------------------------------------------------
-@REM Copyright 2006 The Apache Software Foundation.
-@REM 
-@REM Licensed under the Apache License, Version 2.0 (the "License");
-@REM you may not use this file except in compliance with the License.
-@REM You may obtain a copy of the License at
-@REM 
-@REM      http://www.apache.org/licenses/LICENSE-2.0
-@REM 
-@REM Unless required by applicable law or agreed to in writing, software
-@REM distributed under the License is distributed on an "AS IS" BASIS,
-@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@REM See the License for the specific language governing permissions and
-@REM limitations under the License.
-@REM ----------------------------------------------------------------------------
-@REM 
-@setlocal
-@echo off
-
-SET CUR_DIR=%cd%
-cd /d "%~dp0.."
-SET TUSCANY_HOME=%cd%
-cd /d "%CUR_DIR%"
-echo Building Tuscany ...
-cd /d "%TUSCANY_HOME%"
-call mvn -Dtuscany.home=%TUSCANY_HOME% clean install -Dmaven.test.skip=true
-cd /d "%TUSCANY_HOME%/distribution"
-echo Creating Tuscany distribution ...
-call mvn clean install
-cd /d "%CUR_DIR%"
-@endlocal
