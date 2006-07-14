11692e449bf15ddc451357e6256ac9220901e03f
Jean Sebastien Delfino <jsdelfino@apache.org>
2006-07-14 08:09:01 +0000

    Ported the SupplyChain and HelloWorldWeb samples to the new assembly model
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java@421833 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/sca/recursive/helloworldweb/src/test/java/helloworld/HelloWorldTestCase.java
@@ -0,0 +1,63 @@
+/**
+ *
+ *  Copyright 2005 The Apache Software Foundation or its licensors, as applicable.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package helloworld;
+
+import junit.framework.TestCase;
+
+import org.apache.tuscany.core.client.TuscanyRuntime;
+import org.osoa.sca.CompositeContext;
+import org.osoa.sca.CurrentCompositeContext;
+
+/**
+ * This shows how to test the HelloWorld service component.
+ */
+public class HelloWorldTestCase extends TestCase {
+    
+    private TuscanyRuntime tuscany;
+    
+    protected void setUp() throws Exception {
+        super.setUp();
+        
+        // Create a Tuscany runtime
+        tuscany = new TuscanyRuntime("HelloWorldWebSample", "helloworldweb");
+
+        // Start the Tuscany runtime and associate it with this thread
+        tuscany.start();
+    }
+    
+    public void testGeetings() throws Exception {
+
+        // Get the SCA composite context.
+        CompositeContext compositeContext = CurrentCompositeContext.getContext();
+
+        // Locate the HelloWorld service
+        HelloWorldService helloworldService = compositeContext.locateService(HelloWorldService.class, "HelloWorldServiceComponent");
+        
+        // Invoke the HelloWorld service
+        String value = helloworldService.getGreetings("World");
+        
+        assertEquals(value, "Hello World");
+    }
+    
+    protected void tearDown() throws Exception {
+        
+        // Stop the Tuscany runtime
+        tuscany.stop();
+        
+        super.tearDown();
+    }
+}
