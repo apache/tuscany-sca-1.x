c8092a6ccce7adeb466c9863142dd61e42fa1731
Zhaohui Feng <rfeng@apache.org>
2007-03-13 20:15:38 +0000

    [sca-integration-branch] Fix javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/branches/sca-java-integration@517850 13f79535-47bb-0310-9956-ffa450edef68

--- sca/kernel/api/src/main/java/org/apache/tuscany/api/SCARuntime.java
+++ sca/kernel/api/src/main/java/org/apache/tuscany/api/SCARuntime.java
@@ -44,9 +44,10 @@ public abstract class SCARuntime {
 
     /**
      * Read the service name from a configuration file
+     * 
      * @param classLoader
-     * @param name
-     * @return
+     * @param name The name of the service class
+     * @return A class name which extends/implements the service class
      * @throws IOException
      */
     private static String getServiceName(ClassLoader classLoader, String name) throws IOException {
@@ -74,14 +75,11 @@ public abstract class SCARuntime {
     }
 
     /**
-     * Returns a TuscanyContainer instance. If the system property
-     * "org.apache.tuscany.api.TuscanyContainer" is set, its value is used as
-     * the name of the implementation class. Otherwise, if the resource
-     * "META-INF/services/org.apache.tuscany.api.TuscanyContainer" can be loaded
-     * from the supplied classloader. Otherwise, it will use
-     * "org.apache.tuscany.core.bootstrap.DefaultSCARuntime" as the default.
-     * The named class is loaded from the supplied classloader and instantiated
-     * using its default (no-arg) constructor.
+     * Returns a SCARuntime instance. If the system property "org.apache.tuscany.api.SCARuntime" is set, its value is
+     * used as the name of the implementation class. Otherwise, if the resource
+     * "META-INF/services/org.apache.tuscany.api.SCARuntime" can be loaded from the supplied classloader. Otherwise, it
+     * will use "org.apache.tuscany.core.bootstrap.DefaultSCARuntime" as the default. The named class is loaded from the
+     * supplied classloader and instantiated using its default (no-arg) constructor.
      * 
      * @return
      */
@@ -102,14 +100,14 @@ public abstract class SCARuntime {
                 className = "org.apache.tuscany.core.bootstrap.DefaultSCARuntime";
             }
             Class cls = Class.forName(className, true, classLoader);
-            return (SCARuntime)cls.newInstance();  // NOPMD lresende
+            return (SCARuntime)cls.newInstance(); // NOPMD lresende
         } catch (Exception e) {
             throw new IllegalStateException(e);
         }
     }
 
     /**
-     * Get an instance of Tuscany container
+     * Get an instance of SCA Runtime
      * 
      * @return The instance
      */
@@ -134,7 +132,7 @@ public abstract class SCARuntime {
     }
 
     /**
-     * Start the Tuscany container with the given SCDLs
+     * Start the SCA Runtime with the given SCDLs
      * 
      * @param system The URL for the system SCDL
      * @param extensions An array of URLs for extensions
@@ -149,7 +147,7 @@ public abstract class SCARuntime {
     }
 
     /**
-     * Start the Tuscany container with the given SCDL
+     * Start the SCA Runtime with the given SCDL
      * 
      * @param application The URL for the application SCDL
      */
@@ -162,7 +160,7 @@ public abstract class SCARuntime {
     }
 
     /**
-     * Start the Tuscany container with the given SCDL
+     * Start the SCA Runtime with the given SCDL
      * 
      * @param compositePath The path of the application SCDL
      */
@@ -177,7 +175,7 @@ public abstract class SCARuntime {
     }
 
     /**
-     * Start the Tuscany container with the given SCDL
+     * Start the SCA Runtime with the given SCDL
      * 
      * @param compositePath The path of the system SCDL
      * @param compositePath The path of the application SCDL
@@ -203,16 +201,16 @@ public abstract class SCARuntime {
         return getInstance().getContext(componentName);
     }
 
-    
     /**
      * Get access to a system service
+     * 
      * @param serviceName
      * @return
      */
     protected abstract Object getSystemService(String serviceName);
-    
+
     /**
-     * Stop the Tuscany container
+     * Stop the SCA Runtime
      */
     public static void stop() {
         try {
@@ -220,19 +218,21 @@ public abstract class SCARuntime {
         } catch (Exception e) {
             throw new IllegalStateException(e);
         } finally {
-        	instance = null;
+            instance = null;
         }
     }
 
     /**
      * Look up the ComponentContext by name
+     * 
      * @param componentName
      * @return
      */
     protected abstract ComponentContext getContext(String componentName);
 
     /**
-     * Start up the container
+     * Start up the runtime
+     * 
      * @param system The URL of the SCDL for tuscany system composite
      * @param extensions The URLs of the SCDLs for tuscany extension composites
      * @param application The URL of the SCDL for the application composite
@@ -243,7 +243,8 @@ public abstract class SCARuntime {
         throws Exception;
 
     /**
-     * Shutdown the container
+     * Shutdown the runtime
+     * 
      * @throws Exception
      */
     protected abstract void shutdown() throws Exception;
