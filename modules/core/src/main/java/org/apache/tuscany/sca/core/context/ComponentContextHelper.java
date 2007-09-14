2efb4a1647cf48accdac955ca760bd6b5997a7b3
Zhaohui Feng <rfeng@apache.org>
2007-09-14 22:52:17 +0000

    Merge the fix from trunk: Rename WireableBinding to OptimizableBinding Bring up the cross-databinding exception itest Fix the interface contract for java proxy
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/branches/sca-java-1.0@575812 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/main/java/org/apache/tuscany/sca/core/context/ComponentContextHelper.java
+++ modules/core/src/main/java/org/apache/tuscany/sca/core/context/ComponentContextHelper.java
@@ -42,7 +42,7 @@ import org.apache.tuscany.sca.assembly.Composite;
 import org.apache.tuscany.sca.assembly.Multiplicity;
 import org.apache.tuscany.sca.assembly.Reference;
 import org.apache.tuscany.sca.assembly.Service;
-import org.apache.tuscany.sca.assembly.WireableBinding;
+import org.apache.tuscany.sca.assembly.OptimizableBinding;
 import org.apache.tuscany.sca.contribution.processor.StAXArtifactProcessor;
 import org.apache.tuscany.sca.contribution.processor.StAXArtifactProcessorExtensionPoint;
 import org.apache.tuscany.sca.core.assembly.CompositeActivator;
@@ -96,12 +96,12 @@ public class ComponentContextHelper {
         ComponentReference componentReference = assemblyFactory.createComponentReference();
         componentReference.setName("$self$." + service.getName());
         for (Binding binding : service.getBindings()) {
-            if (binding instanceof WireableBinding) {
-                WireableBinding wireableBinding = (WireableBinding)((WireableBinding)binding).clone();
-                wireableBinding.setTargetBinding(binding);
-                wireableBinding.setTargetComponent(component);
-                wireableBinding.setTargetComponentService(service);
-                componentReference.getBindings().add(wireableBinding);
+            if (binding instanceof OptimizableBinding) {
+                OptimizableBinding optimizableBinding = (OptimizableBinding)((OptimizableBinding)binding).clone();
+                optimizableBinding.setTargetBinding(binding);
+                optimizableBinding.setTargetComponent(component);
+                optimizableBinding.setTargetComponentService(service);
+                componentReference.getBindings().add(optimizableBinding);
             } else {
                 componentReference.getBindings().add(binding);
             }
@@ -134,14 +134,19 @@ public class ComponentContextHelper {
     private InterfaceContract getInterfaceContract(InterfaceContract interfaceContract, Class<?> businessInterface)
         throws CloneNotSupportedException, InvalidInterfaceException {
         Interface interfaze = interfaceContract.getInterface();
+        boolean compatible = false;
         if (interfaze instanceof JavaInterface) {
             Class<?> cls = ((JavaInterface)interfaze).getJavaClass();
-            if (!businessInterface.isAssignableFrom(cls)) {
-                // The interface is not assignable from the interface contract
-                interfaceContract = (InterfaceContract)interfaceContract.clone();
-                interfaceContract.setInterface(javaInterfaceFactory.createJavaInterface(businessInterface));
+            if (businessInterface.isAssignableFrom(cls)) {
+                compatible = true;
             }
         }
+        if (!compatible) {
+            // The interface is not assignable from the interface contract
+            interfaceContract = (InterfaceContract)interfaceContract.clone();
+            interfaceContract.setInterface(javaInterfaceFactory.createJavaInterface(businessInterface));
+        }
+
         return interfaceContract;
     }
 
@@ -175,12 +180,12 @@ public class ComponentContextHelper {
         ref.getTargets().add(service);
         ref.getBindings().clear();
         for (Binding binding : service.getBindings()) {
-            if (binding instanceof WireableBinding) {
-                WireableBinding wireableBinding = (WireableBinding)((WireableBinding)binding).clone();
-                wireableBinding.setTargetBinding(binding);
-                wireableBinding.setTargetComponent(component);
-                wireableBinding.setTargetComponentService(service);
-                ref.getBindings().add(wireableBinding);
+            if (binding instanceof OptimizableBinding) {
+                OptimizableBinding optimizableBinding = (OptimizableBinding)((OptimizableBinding)binding).clone();
+                optimizableBinding.setTargetBinding(binding);
+                optimizableBinding.setTargetComponent(component);
+                optimizableBinding.setTargetComponentService(service);
+                ref.getBindings().add(optimizableBinding);
             } else {
                 ref.getBindings().add(binding);
             }
