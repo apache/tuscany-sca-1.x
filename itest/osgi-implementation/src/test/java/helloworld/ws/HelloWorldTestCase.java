fe22e9d3faffb0754254dcca58b6aefa2faf05be
Zhaohui Feng <rfeng@apache.org>
2007-09-17 08:25:36 +0000

    Exclude the javax.servlet from felix as it causes conflict with Jetty (NoScuhMethodError for ServletRequest.getContaxtPath())
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/branches/sca-java-1.0@576297 13f79535-47bb-0310-9956-ffa450edef68

--- itest/osgi-implementation/src/test/java/helloworld/ws/HelloWorldTestCase.java
+++ itest/osgi-implementation/src/test/java/helloworld/ws/HelloWorldTestCase.java
@@ -27,7 +27,7 @@ import org.apache.tuscany.sca.host.embedded.SCADomain;
 import util.OSGiTestUtil;
 
 
-public class HelloWorldTestCaseFIXME extends TestCase {
+public class HelloWorldTestCase extends TestCase {
 
     private SCADomain scaDomain;
     private HelloWorld helloWorld;
