1d6a36431261a4c1eccde4090dc2ea76e0124c7c
Zhaohui Feng <rfeng@apache.org>
2008-07-02 15:37:05 +0000

    Fix for TUSCANY-2444, merged from trunk
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-java-1.3@673421 13f79535-47bb-0310-9956-ffa450edef68

--- modules/interface-java-jaxws/src/main/java/org/apache/tuscany/sca/interfacedef/java/jaxws/WrapperBeanGenerator.java
+++ modules/interface-java-jaxws/src/main/java/org/apache/tuscany/sca/interfacedef/java/jaxws/WrapperBeanGenerator.java
@@ -46,7 +46,7 @@ public class WrapperBeanGenerator extends BaseBeanGenerator {
         String wrapperNamespace = JavaInterfaceUtil.getNamespace(sei);
         String wrapperName = m.getName();
         String wrapperBeanName = capitalize(wrapperName);
-        String wrapperClassName = sei.getPackage().getName() + ".jaxws." + wrapperBeanName;
+        String wrapperClassName = CodeGenerationHelper.getPackagePrefix(sei) + wrapperBeanName;
 
         return generateRequestWrapper(m, wrapperClassName, wrapperNamespace, wrapperName, cl);
     }
@@ -90,7 +90,7 @@ public class WrapperBeanGenerator extends BaseBeanGenerator {
 
         String wrapperName = m.getName() + "Response";
         String wrapperBeanName = capitalize(wrapperName);
-        String wrapperClassName = sei.getPackage().getName() + ".jaxws." + wrapperBeanName;
+        String wrapperClassName = CodeGenerationHelper.getPackagePrefix(sei) + wrapperBeanName;
         return generateResponseWrapper(m, wrapperClassName, wrapperNamespace, wrapperName, cl);
 
     }
