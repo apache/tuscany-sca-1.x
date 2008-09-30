34267b9168b4358f59065de4f2350d8f1b95c576
Simon Laws <slaws@apache.org>
2008-09-30 15:01:28 +0000

    TUSCANY-2628 Change some ASL2 licenses to OSOA licenses to cover material that comes from the OSOA specs
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-java-1.3.2@700490 13f79535-47bb-0310-9956-ffa450edef68

--- modules/sca-api/src/main/java/org/osoa/sca/ServiceUnavailableException.java
+++ modules/sca-api/src/main/java/org/osoa/sca/ServiceUnavailableException.java
@@ -1,20 +1,25 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.    
+ * Permission to copy, display and distribute the Service Component Architecture Specification and/or
+ * portions thereof, without modification, in any medium without fee or royalty is hereby granted, provided
+ * that you include the following on ALL copies of the Service Component Architecture Specification, or
+ * portions thereof, that you make:
+ * 1. A link or URL to the Service Component Architecture Specification at this location:
+ *    http://www.osoa.org/display/Main/Service+Component+Architecture+Specifications
+ * 2. The full text of the copyright notice as shown in the Service Component Architecture Specification.
+ * BEA, Cape Clear, IBM, Interface21, IONA, Oracle, Primeton, Progress Software, Red Hat, Rogue Wave,
+ * SAP, Siemens, Software AG., Sun, Sybase, TIBCO (collectively, the "Authors") agree to grant you a
+ * royalty-free license, under reasonable, non-discriminatory terms and conditions to patents that they deem
+ * necessary to implement the Service Component Architecture Specification.
+ * THE Service Component Architecture SPECIFICATION IS PROVIDED "AS IS," AND THE
+ * AUTHORS MAKE NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED,
+ * REGARDING THIS SPECIFICATION AND THE IMPLEMENTATION OF ITS CONTENTS,
+ * INCLUDING, BUT NOT LIMITED TO, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, NON-INFRINGEMENT OR TITLE.
+ * THE AUTHORS WILL NOT BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL
+ * OR CONSEQUENTIAL DAMAGES ARISING OUT OF OR RELATING TO ANY USE OR
+ * DISTRIBUTION OF THE Service Components Architecture SPECIFICATION.
+ * The name and trademarks of the Authors may NOT be used in any manner, including advertising or
+ * publicity pertaining to the Service Component Architecture Specification or its contents without specific,   
  */
 package org.osoa.sca;
 
