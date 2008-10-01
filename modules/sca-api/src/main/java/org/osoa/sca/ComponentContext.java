e521934a1092c9c50f38211e663cbe9d7f18dcaa
Simon Laws <slaws@apache.org>
2008-10-01 20:14:15 +0000

    TUSCANY-2620, TUSCANY-2628 - correct my last change for these JIRA by only including the required license and copyright statement in the files. Also I notice that the copyright is different between the SCA Java Annotations and APIs spec and the SCA JEE  spec
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-java-1.3.2@700902 13f79535-47bb-0310-9956-ffa450edef68

--- modules/sca-api/src/main/java/org/osoa/sca/ComponentContext.java
+++ modules/sca-api/src/main/java/org/osoa/sca/ComponentContext.java
@@ -1,25 +1,9 @@
 /*
- * Permission to copy, display and distribute the Service Component Architecture Specification and/or
- * portions thereof, without modification, in any medium without fee or royalty is hereby granted, provided
- * that you include the following on ALL copies of the Service Component Architecture Specification, or
- * portions thereof, that you make:
- * 1. A link or URL to the Service Component Architecture Specification at this location:
- *    http://www.osoa.org/display/Main/Service+Component+Architecture+Specifications
- * 2. The full text of the copyright notice as shown in the Service Component Architecture Specification.
- * BEA, Cape Clear, IBM, Interface21, IONA, Oracle, Primeton, Progress Software, Red Hat, Rogue Wave,
- * SAP, Siemens, Software AG., Sun, Sybase, TIBCO (collectively, the "Authors") agree to grant you a
- * royalty-free license, under reasonable, non-discriminatory terms and conditions to patents that they deem
- * necessary to implement the Service Component Architecture Specification.
- * THE Service Component Architecture SPECIFICATION IS PROVIDED "AS IS," AND THE
- * AUTHORS MAKE NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED,
- * REGARDING THIS SPECIFICATION AND THE IMPLEMENTATION OF ITS CONTENTS,
- * INCLUDING, BUT NOT LIMITED TO, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE, NON-INFRINGEMENT OR TITLE.
- * THE AUTHORS WILL NOT BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL
- * OR CONSEQUENTIAL DAMAGES ARISING OUT OF OR RELATING TO ANY USE OR
- * DISTRIBUTION OF THE Service Components Architecture SPECIFICATION.
- * The name and trademarks of the Authors may NOT be used in any manner, including advertising or
- * publicity pertaining to the Service Component Architecture Specification or its contents without specific,   
+ * (c) Copyright BEA Systems, Inc., Cape Clear Software, International Business Machines Corp, Interface21, IONA Technologies,
+ * Oracle, Primeton Technologies, Progress Software, Red Hat, Rogue Wave Software, SAP AG., Siemens AG., Software AG., Sybase
+ * Inc., TIBCO Software Inc., 2005, 2007. All rights reserved.
+ * 
+ * see http://www.osoa.org/display/Main/Service+Component+Architecture+Specifications
  */
 package org.osoa.sca;
 
