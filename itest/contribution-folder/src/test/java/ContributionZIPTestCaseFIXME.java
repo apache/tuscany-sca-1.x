0bed76b0ba150cd5896bce7fc206d5dad81149f5
Anthony Elder <antelder@apache.org>
2009-01-23 09:28:36 +0000

    Add an itest for folder contributions and zip contributions containing jars
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-java-1.x@736977 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/contribution-folder/src/test/java/ContributionZIPTestCaseFIXME.java
@@ -0,0 +1,35 @@
+
+
+import junit.framework.TestCase;
+
+import org.apache.tuscany.sca.host.embedded.SCADomain;
+
+import calculator.CalculatorService;
+
+
+
+/**
+ * Test SCADomain.newInstance and invocation of a service.
+ * 
+ * @version $Rev: 608205 $ $Date: 2008-01-02 20:29:05 +0000 (Wed, 02 Jan 2008) $
+ */
+public class ContributionZIPTestCaseFIXME extends TestCase {
+
+    private SCADomain domain;
+    
+    @Override
+    protected void setUp() throws Exception {
+        domain = SCADomain.newInstance("myDomain", "src/test/resources/repository3/contribution.zip", null );
+    }
+
+    public void testInvoke() throws Exception {
+        CalculatorService service = domain.getService(CalculatorService.class, "CalculatorServiceComponent");
+        assertEquals(3.0, service.add(1, 2));
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        domain.close();
+    }
+
+}
