df557495f27fac53079383a0dd3c45bd1d699e91
Anthony Elder <antelder@apache.org>
2009-02-17 11:54:53 +0000

    TUSCANY-2839: Add checks to bypass response processing for oneway operations
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-java-1.x/modules@744971 13f79535-47bb-0310-9956-ffa450edef68

--- binding-jms-runtime/src/main/java/org/apache/tuscany/sca/binding/jms/wireformat/jmstextxml/runtime/WireFormatJMSTextXMLServiceInterceptor.java
+++ binding-jms-runtime/src/main/java/org/apache/tuscany/sca/binding/jms/wireformat/jmstextxml/runtime/WireFormatJMSTextXMLServiceInterceptor.java
@@ -18,19 +18,15 @@
  */
 package org.apache.tuscany.sca.binding.jms.wireformat.jmstextxml.runtime;
 
-import javax.jms.JMSException;
 import javax.jms.Session;
-import javax.naming.NamingException;
 
-import org.apache.tuscany.sca.assembly.WireFormat;
 import org.apache.tuscany.sca.binding.jms.context.JMSBindingContext;
 import org.apache.tuscany.sca.binding.jms.impl.JMSBinding;
-import org.apache.tuscany.sca.binding.jms.impl.JMSBindingConstants;
-import org.apache.tuscany.sca.binding.jms.impl.JMSBindingException;
 import org.apache.tuscany.sca.binding.jms.provider.JMSMessageProcessor;
 import org.apache.tuscany.sca.binding.jms.provider.JMSMessageProcessorUtil;
 import org.apache.tuscany.sca.binding.jms.provider.JMSResourceFactory;
 import org.apache.tuscany.sca.binding.jms.wireformat.jmstextxml.WireFormatJMSTextXML;
+import org.apache.tuscany.sca.interfacedef.Operation;
 import org.apache.tuscany.sca.invocation.Interceptor;
 import org.apache.tuscany.sca.invocation.Invoker;
 import org.apache.tuscany.sca.invocation.Message;
@@ -67,6 +63,12 @@ public class WireFormatJMSTextXMLServiceInterceptor implements Interceptor {
         
         msg = getNext().invoke(msg);
         
+        //if it's oneway return back
+        Operation operation = msg.getOperation();
+        if (operation != null && operation.isNonBlocking()) {
+            return msg;
+        }
+
         if (jmsBinding.getResponseWireFormat() instanceof WireFormatJMSTextXML){
             msg = invokeResponse(msg);
         }
