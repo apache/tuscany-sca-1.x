bdba1db585b56f948102ff45e7c61437ff91313f
Zhaohui Feng <rfeng@apache.org>
2009-03-28 00:11:26 +0000

    Add fault to WSDL
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sandbox/travelsample@759407 13f79535-47bb-0310-9956-ffa450edef68

--- shared-contributions/payment-java-contribution/src/main/java/payment/PaymentImpl.java
+++ shared-contributions/payment-java-contribution/src/main/java/payment/PaymentImpl.java
@@ -6,24 +6,28 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package payment;
+
+import javax.annotation.security.RolesAllowed;
+import javax.annotation.security.RunAs;
 
 import org.osoa.sca.annotations.Authentication;
 import org.osoa.sca.annotations.Property;
 import org.osoa.sca.annotations.Reference;
 import org.osoa.sca.annotations.Service;
 
+import payment.creditcard.AuthorizeFault_Exception;
 import payment.creditcard.CreditCardDetailsType;
 import payment.creditcard.CreditCardPayment;
 import scatours.customer.Customer;
@@ -35,6 +39,8 @@ import scatours.emailgateway.EmailGateway;
  * The payment implementation
  */
 @Service(Payment.class)
+@RolesAllowed({"Admin", "Billing"})
+@RunAs("Billing")
 public class PaymentImpl implements Payment {
 
     @Reference
@@ -52,18 +58,23 @@ public class PaymentImpl implements Payment {
 
     public String makePaymentMember(String customerId, float amount) {
         Customer customer = null;
-        
+
         try {
             customer = customerRegistry.getCustomer(customerId);
         } catch (CustomerNotFoundException ex) {
             return "Payment failed due to " + ex.getMessage();
-        } catch (Throwable t) {   
+        } catch (Throwable t) {
             return "Payment failed due to system error " + t.getMessage();
         }
 
         CreditCardDetailsType ccDetails = customer.getCreditCard();
 
-        String status = creditCardPayment.authorize(ccDetails, amount);
+        String status;
+        try {
+            status = creditCardPayment.authorize(ccDetails, amount);
+        } catch (AuthorizeFault_Exception e) {
+            status = e.getFaultInfo().getErrorCode();
+        }
 
         StringBuffer body = new StringBuffer();
         body.append(customer);
--- shared-contributions/payment-java-contribution/src/main/java/payment/creditcard/CreditCardPaymentImpl.java
+++ shared-contributions/payment-java-contribution/src/main/java/payment/creditcard/CreditCardPaymentImpl.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package payment.creditcard;
@@ -29,7 +29,7 @@ import org.osoa.sca.annotations.Service;
 @RolesAllowed("Admin")
 public class CreditCardPaymentImpl implements CreditCardPayment {
 
-    public String authorize(CreditCardDetailsType creditCard, float amount) {
+    public String authorize(CreditCardDetailsType creditCard, float amount) throws AuthorizeFault_Exception {
         if (creditCard != null) {
             System.out.println("Checking card: name = " + creditCard.getCardOwner().getName()
                 + " number = "
@@ -38,6 +38,11 @@ public class CreditCardPaymentImpl implements CreditCardPayment {
                 + amount);
         } else {
             System.out.println("Checking card is null");
+            ObjectFactory factory = new ObjectFactory();
+            AuthorizeFault fault = factory.createAuthorizeFault();
+            fault.setErrorCode("001 - Invalid card");
+            AuthorizeFault_Exception ex = new AuthorizeFault_Exception("Invalid card", fault);
+            throw ex;
         }
 
         return "OK";
