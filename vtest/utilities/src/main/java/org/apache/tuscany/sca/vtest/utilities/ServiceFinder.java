7683d1f41c588f9de675bce93a1df31f049db4d8
Zhaohui Feng <rfeng@apache.org>
2009-04-02 00:08:10 +0000

    Make sure SCA domain is closed
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-java-1.x@761116 13f79535-47bb-0310-9956-ffa450edef68

--- vtest/utilities/src/main/java/org/apache/tuscany/sca/vtest/utilities/ServiceFinder.java
+++ vtest/utilities/src/main/java/org/apache/tuscany/sca/vtest/utilities/ServiceFinder.java
@@ -5,15 +5,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package org.apache.tuscany.sca.vtest.utilities;
@@ -30,8 +30,8 @@ import org.apache.tuscany.sca.assembly.ComponentService;
 import org.apache.tuscany.sca.host.embedded.SCADomain;
 
 /**
- * 
- * 
+ *
+ *
  */
 public class ServiceFinder {
 
@@ -42,66 +42,65 @@ public class ServiceFinder {
     }
 
     public static void init(String compositeFileName) {
-        if (domain != null)
+        if (domain != null) {
             System.out.println("VTEST WARNING: domain already exists and is will be overwritten!");
+            domain.close();
+        }
         domain = SCADomain.newInstance(compositeFileName);
-    } 
-    
-    
+    }
+
     public static <B> B getService(Class<B> businessInterface, String serviceName) {
         return domain.getService(businessInterface, serviceName);
     }
 
     public static void cleanup() {
-        if (domain != null){
-            domain.close(); 
+        if (domain != null) {
+            domain.close();
         }
-      
         domain = null;
     }
 
     private static String getUri(String component, String service, String binding) {
-        
-    	List<ComponentService> csList = domain.getComponentManager().getComponent(component).getServices();
-    	
-    	for (int i = 0; i < csList.size(); i++) {
-    		ComponentService cs = csList.get(i);
-    		if (service.equals(cs.getName())) {
-        		List<Binding> bList = cs.getBindings();
-        		for (int j = 0; j < bList.size(); j++) {
-        			String bName = bList.get(j).getName();
-        			if (bName.equals(binding)) {
-            			String bUri  = bList.get(j).getURI();
-            			System.out.println(component + "/" + service + "-> binding name: " + bName + ", uri: " + bUri);
-            			return bUri;
-        			}
-        		}
-    		}
-    	}
-    	return null;
+
+        List<ComponentService> csList = domain.getComponentManager().getComponent(component).getServices();
+
+        for (int i = 0; i < csList.size(); i++) {
+            ComponentService cs = csList.get(i);
+            if (service.equals(cs.getName())) {
+                List<Binding> bList = cs.getBindings();
+                for (int j = 0; j < bList.size(); j++) {
+                    String bName = bList.get(j).getName();
+                    if (bName.equals(binding)) {
+                        String bUri = bList.get(j).getURI();
+                        System.out.println(component + "/" + service + "-> binding name: " + bName + ", uri: " + bUri);
+                        return bUri;
+                    }
+                }
+            }
+        }
+        return null;
     }
-    
+
     public static Definition getWSDLDefinition(String component, String service) {
-    	return getWSDLDefinition(component, service, service);
+        return getWSDLDefinition(component, service, service);
     }
-    	
+
     public static Definition getWSDLDefinition(String component, String service, String binding) {
 
-    	String uri = getUri(component, service, binding);
+        String uri = getUri(component, service, binding);
 
         if (uri == null)
-    		return null;
-    	
-    	try {
-    		WSDLReader wsdlReader = WSDLFactory.newInstance().newWSDLReader();
-            wsdlReader.setFeature("javax.wsdl.verbose",false);
-            wsdlReader.setFeature("javax.wsdl.importDocuments",true);
-            return wsdlReader.readWSDL(uri + "?wsdl");
-    	} catch (WSDLException e) {
-    		e.printStackTrace(System.out);
-    	}
-    	return null;
-    }
+            return null;
 
+        try {
+            WSDLReader wsdlReader = WSDLFactory.newInstance().newWSDLReader();
+            wsdlReader.setFeature("javax.wsdl.verbose", false);
+            wsdlReader.setFeature("javax.wsdl.importDocuments", true);
+            return wsdlReader.readWSDL(uri + "?wsdl");
+        } catch (WSDLException e) {
+            e.printStackTrace(System.out);
+        }
+        return null;
+    }
 
 }
