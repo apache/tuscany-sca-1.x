11ef65cdb02b3e2edde41f4af15e8a73a3e66cce
Zhaohui Feng <rfeng@apache.org>
2009-04-04 08:28:58 +0000

    Comment out the test case due to TUSCANY-2957
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-java-1.x@761876 13f79535-47bb-0310-9956-ffa450edef68

--- modules/binding-ejb-runtime/src/test/java/org/apache/tuscany/sca/binding/ejb/tests/EJBReferenceTestCase.java
+++ modules/binding-ejb-runtime/src/test/java/org/apache/tuscany/sca/binding/ejb/tests/EJBReferenceTestCase.java
@@ -6,35 +6,38 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package org.apache.tuscany.sca.binding.ejb.tests;
 
-import junit.framework.TestCase;
-
 import org.apache.tuscany.sca.host.embedded.SCADomain;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 
 import account.Customer;
 
 /**
- * Invokes the component which calls the reference using the EJB binding 
+ * Invokes the component which calls the reference using the EJB binding
  *
  * @version $Rev$ $Date$
  */
-public class EJBReferenceTestCase extends TestCase {
+public class EJBReferenceTestCase {
     private static final int MOCK_PORT = 8085;
     private SCADomain scaDomain;
 
-    @Override
-    protected void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         System.setProperty("java.naming.factory.initial", "org.apache.openejb.client.RemoteInitialContextFactory");
         System.setProperty("java.naming.provider.url", "ejbd://localhost:" + MOCK_PORT);
         System.setProperty("managed", "false");
@@ -46,22 +49,24 @@ public class EJBReferenceTestCase extends TestCase {
 
         // Start the mock server to simulate the remote EJB
         new Thread(new MockServer(MOCK_PORT)).start();
-        
+
         // Wait enough for the server to be started
         Thread.sleep(500);
     }
 
-    @Override
-    protected void tearDown() throws Exception {
+    @After
+    public void tearDown() throws Exception {
         scaDomain.close();
     }
 
+    @Ignore("TUSCANY-2957")
+    @Test
     public void testCalculator() throws Exception {
         Customer customer = scaDomain.getService(Customer.class, "CustomerComponent");
         // This is one of the customer numbers in bank application running on Geronimo
         String accountNo = "1234567890";
         Double balance = customer.depositAmount(accountNo, new Double(100));
         // System.out.println("Balance amount for account " + accountNo + " is $" + balance);
-        assertEquals(1200.0, balance);
+        Assert.assertEquals(1200.0, balance, 0.0);
     }
 }
