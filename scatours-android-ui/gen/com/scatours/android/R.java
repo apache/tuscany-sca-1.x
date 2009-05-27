ccc327f1c155a6c56f1163f655e980f763031e3e
Luciano Resende <lresende@apache.org>
2009-05-27 16:05:26 +0000

    Starting to add logic to search button listeners
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sandbox/travelsample@779219 13f79535-47bb-0310-9956-ffa450edef68

--- scatours-android-ui/gen/com/scatours/android/R.java
+++ scatours-android-ui/gen/com/scatours/android/R.java
@@ -27,6 +27,8 @@ public final class R {
         public static final int test=0x7f030001;
     }
     public static final class string {
+        public static final int alert_cancel=0x7f040003;
+        public static final int alert_yes=0x7f040002;
         public static final int app_name=0x7f040000;
         public static final int title_search=0x7f040001;
     }
--- scatours-android-ui/src/com/scatours/android/TripSearch.java
+++ scatours-android-ui/src/com/scatours/android/TripSearch.java
@@ -1,9 +1,15 @@
 package com.scatours.android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 public class TripSearch extends Activity {
     /** Called when the activity is first created. */
@@ -22,6 +28,33 @@ public class TripSearch extends Activity {
         AutoCompleteTextView textViewTo = (AutoCompleteTextView) findViewById(R.id.edit_toLocation);
         //ArrayAdapter adapterTo = new ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line, AIRPORT_CODES);
         textViewTo.setAdapter(adapter);
+        
+        
+        Button closeButton = (Button) this.findViewById(R.id.btnSearch);
+        closeButton.setOnClickListener( new OnClickListener() {
+            public void onClick(View v) {
+                //Search trips here
+                new AlertDialog.Builder(TripSearch.this)
+                .setTitle("SCATour")
+                .setMessage("You're about to search for trips !")
+                .setIcon(R.drawable.icon)
+                .setPositiveButton(R.string.alert_yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        
+                    }})
+                    .setNegativeButton(R.string.alert_cancel, new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dialog, int which) {
+                        
+                        }       
+                    })
+                    .show();
+
+
+
+
+            }
+        });
+        
     }
 
     static final String[] AIRPORT_CODES = new String[] {
