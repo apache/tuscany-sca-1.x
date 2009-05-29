197c88d0e8b1586d06453f0f971e89d565af1065
Zhaohui Feng <rfeng@apache.org>
2009-05-29 20:24:38 +0000

    Change to localhost
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sandbox/travelsample@780097 13f79535-47bb-0310-9956-ffa450edef68

--- launchers/fullapp-launcher/src/main/java/scatours/LaunchFullAppNode.java
+++ launchers/fullapp-launcher/src/main/java/scatours/LaunchFullAppNode.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package scatours;
@@ -29,12 +29,12 @@ public class LaunchFullAppNode {
     public static void main(String[] args) throws Exception {
         LaunchFullAppNode.launchFromFileSystemDir();
     }
-    
-    // OK for development but you must launch the node from this module 
+
+    // OK for development but you must launch the node from this module
     public static void launchFromFileSystemDir(){
-        
+
         try {
-        	SCANode node1 = SCANodeFactory.newInstance().createSCANode(null, 
+        	SCANode node1 = SCANodeFactory.newInstance().createSCANode(null,
 	           new SCAContribution("common", "../../contributions/common-contribution/target/classes"),
 	           new SCAContribution("currency", "../../contributions/currency-contribution/target/classes"),
 	           new SCAContribution("hotel", "../../contributions/hotel-contribution/target/classes"),
@@ -48,20 +48,20 @@ public class LaunchFullAppNode {
 	           new SCAContribution("shoppingcart", "../../contributions/shoppingcart-contribution/target/classes"),
 	           new SCAContribution("scatours", "../../contributions/scatours-contribution/target/classes"),
 	           new SCAContribution("ui", "../../contributions/fullapp-ui-contribution/target/classes"));
-        	
+
             node1.start();
-            
-            System.out.println("Point your browser at - http://L3AW203:8080/scatours/ ");
+
+            System.out.println("Point your browser at - http://localhost:8080/scatours/ ");
             System.out.println("Node started - Press enter to shutdown.");
-            
+
             try {
                 System.in.read();
             } catch (IOException e) {}
-            
+
             node1.stop();
-            
+
         } catch (Exception ex) {
             System.out.println(ex.toString());
         }
-    }    
+    }
 }
