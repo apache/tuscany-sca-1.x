5022fdca4d62eac482b8510de8fca62b58a01fe6
Mark Combellack <mcombellack@apache.org>
2009-06-20 13:52:41 +0000

    Removed unused import
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-java-1.x@786824 13f79535-47bb-0310-9956-ffa450edef68

--- samples/feed-aggregator/src/test/java/feed/FeedAggregatorTestCase.java
+++ samples/feed-aggregator/src/test/java/feed/FeedAggregatorTestCase.java
@@ -44,7 +44,6 @@ import org.apache.abdera.writer.WriterFactory;
 import org.apache.tuscany.sca.host.embedded.SCADomain;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
