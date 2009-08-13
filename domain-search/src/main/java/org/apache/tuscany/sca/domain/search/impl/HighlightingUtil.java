1e881c65437b3963ade76f86da6a3549a1b8fe7b
Luciano Resende <lresende@apache.org>
2009-08-13 20:35:28 +0000

    Adding Apache License headers to domain search artifacts
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-java-1.x/modules@804022 13f79535-47bb-0310-9956-ffa450edef68

--- domain-search/src/main/java/org/apache/tuscany/sca/domain/search/impl/HighlightingUtil.java
+++ domain-search/src/main/java/org/apache/tuscany/sca/domain/search/impl/HighlightingUtil.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
 package org.apache.tuscany.sca.domain.search.impl;
 
 import java.io.IOException;
@@ -14,6 +32,10 @@ import org.apache.lucene.search.highlight.SimpleFragmenter;
 import org.apache.lucene.search.highlight.SpanScorer;
 import org.apache.tuscany.sca.domain.search.Result;
 
+/**
+ * 
+ * @version $Rev$ $Date$
+ */
 final public class HighlightingUtil {
 	
 	public static void highlightResult(Result result, Query query) {
