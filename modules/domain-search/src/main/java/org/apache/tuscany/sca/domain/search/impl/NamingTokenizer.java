4f6dec8eddb423275b235ecf574d19c1601e3c8c
Luciano Resende <lresende@apache.org>
2009-08-13 20:35:28 +0000

    Adding Apache License headers to domain search artifacts
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-java-1.x@804022 13f79535-47bb-0310-9956-ffa450edef68

--- modules/domain-search/src/main/java/org/apache/tuscany/sca/domain/search/impl/NamingTokenizer.java
+++ modules/domain-search/src/main/java/org/apache/tuscany/sca/domain/search/impl/NamingTokenizer.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
 package org.apache.tuscany.sca.domain.search.impl;
 
 import java.io.IOException;
@@ -6,6 +24,10 @@ import java.io.Reader;
 import org.apache.lucene.analysis.Token;
 import org.apache.lucene.analysis.Tokenizer;
 
+/**
+ * 
+ * @version $Rev$ $Date$
+ */
 public class NamingTokenizer extends Tokenizer {
 
 	private int offset = 0, bufferIndex = 0, dataLen = 0;
