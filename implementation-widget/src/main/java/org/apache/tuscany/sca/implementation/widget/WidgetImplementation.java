75ac52b3ee7c22258f23b9720103787d08886fa7
Luciano Resende <lresende@apache.org>
2009-09-03 03:59:59 +0000

    TUSCANY-3247 - Making interfaces and instantiatng widget factory via extension point mechanism
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-java-1.x/modules@810774 13f79535-47bb-0310-9956-ffa450edef68

--- implementation-widget/src/main/java/org/apache/tuscany/sca/implementation/widget/WidgetImplementation.java
+++ implementation-widget/src/main/java/org/apache/tuscany/sca/implementation/widget/WidgetImplementation.java
@@ -19,20 +19,8 @@
 package org.apache.tuscany.sca.implementation.widget;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
-import org.apache.tuscany.sca.assembly.AssemblyFactory;
-import org.apache.tuscany.sca.assembly.ConstrainingType;
 import org.apache.tuscany.sca.assembly.Implementation;
-import org.apache.tuscany.sca.assembly.Property;
-import org.apache.tuscany.sca.assembly.Reference;
-import org.apache.tuscany.sca.assembly.Service;
-import org.apache.tuscany.sca.interfacedef.InvalidInterfaceException;
-import org.apache.tuscany.sca.interfacedef.java.JavaInterface;
-import org.apache.tuscany.sca.interfacedef.java.JavaInterfaceContract;
-import org.apache.tuscany.sca.interfacedef.java.JavaInterfaceFactory;
 
 
 /**
@@ -40,95 +28,13 @@ import org.apache.tuscany.sca.interfacedef.java.JavaInterfaceFactory;
  *
  * @version $Rev$ $Date$
  */
-public class WidgetImplementation implements Implementation {
-    private Service widgetService;
-    private List<Reference> references = new ArrayList<Reference>();
-    private List<Property> properties = new ArrayList<Property>();
+public interface WidgetImplementation extends Implementation {
     
-    private String location;
-    private URL url;
-    private boolean unresolved;
+    String getLocation();
 
-    /**
-     * Constructs a new resource implementation.
-     */
-    WidgetImplementation(AssemblyFactory assemblyFactory,
-                         JavaInterfaceFactory javaFactory) {
+    void setLocation(String location);
 
-        // Resource implementation always provide a single service exposing
-        // the Resource interface, and have no references and properties
-        widgetService = assemblyFactory.createService();
-        widgetService.setName("Widget");
-        
-        // Create the Java interface contract for the Resource service
-        JavaInterface javaInterface;
-        try {
-            javaInterface = javaFactory.createJavaInterface(Widget.class);
-        } catch (InvalidInterfaceException e) {
-            throw new IllegalArgumentException(e);
-        }
-        JavaInterfaceContract interfaceContract = javaFactory.createJavaInterfaceContract();
-        interfaceContract.setInterface(javaInterface);
-        widgetService.setInterfaceContract(interfaceContract);
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public URL getLocationURL() {
-        return url;
-    }
+    URL getLocationURL();
     
-    public void setLocationURL(URL url) {
-        this.url = url;
-    }
-    
-    public ConstrainingType getConstrainingType() {
-        // The resource implementation does not support constrainingTypes
-        return null;
-    }
-
-    public List<Property> getProperties() {
-        return properties;
-    }
-
-    public List<Service> getServices() {
-        // The resource implementation does not support services
-        return Collections.singletonList(widgetService);
-    }
-    
-    public List<Reference> getReferences() {
-        return references;
-    }
-
-    public String getURI() {
-        return location;
-    }
-
-    public void setConstrainingType(ConstrainingType constrainingType) {
-        // The resource implementation does not support constrainingTypes
-    }
-
-    public void setURI(String uri) {
-        this.location = uri;
-    }
-
-
-    public boolean isUnresolved() {
-        return unresolved;
-    }
-
-    public void setUnresolved(boolean unresolved) {
-        this.unresolved = unresolved;
-    }    
-
-    @Override
-    public String toString() {
-        return "Widget : " + getLocation(); 
-    }
+    void setLocationURL(URL url);
 }
