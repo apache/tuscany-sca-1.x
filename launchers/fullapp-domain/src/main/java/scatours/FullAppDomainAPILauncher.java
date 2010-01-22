2dbca7fb4acfb11e1a5d3d6fccf6a48383cabc1b
Simon Nash <nash@apache.org>
2010-01-22 11:15:58 +0000

    Make domain launchers run from the distribution (TUSCANY-3423)
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sandbox/travelsample@902059 13f79535-47bb-0310-9956-ffa450edef68

--- launchers/fullapp-domain/src/main/java/scatours/FullAppDomainAPILauncher.java
+++ launchers/fullapp-domain/src/main/java/scatours/FullAppDomainAPILauncher.java
@@ -1,4 +1,4 @@
-<!--
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,28 +6,37 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
--->
+ */
 
-<project name="scatours-launcher-introducing-tours" default="run">
-    <import file="../../antdefs.xml"/>
+package scatours;
 
-    <target name="run">
-        <java classname="org.apache.tuscany.sca.node.launcher.NodeLauncher" fork="true">
-            <arg value="http://localhost:9990/node-config/ToursNode"/>
-            <classpath>
-                <fileset dir="${env.TUSCANY_HOME}/modules">
-                    <include name="tuscany-node-launcher-*.jar"/>
-                </fileset>
-            </classpath>
-        </java>
-    </target>
-</project>
+import java.io.IOException;
+import org.apache.tuscany.sca.node.launcher.DomainManagerLauncher;
+
+public class FullAppDomainAPILauncher {
+
+    public static void main(String[] args) throws Exception {
+    	DomainManagerLauncher dml = DomainManagerLauncher.newInstance();
+    	Object dm = dml.createDomainManager("../../domainconfig/fullapp/target/classes");
+        dm.getClass().getMethod("start").invoke(dm);
+    	
+        System.out.println("Point your browser at - http://localhost:9990/ui/home/ ");
+        System.out.println("Domain started - Press enter to shutdown.");
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+        }
+
+        dm.getClass().getMethod("stop").invoke(dm);
+    }
+}
