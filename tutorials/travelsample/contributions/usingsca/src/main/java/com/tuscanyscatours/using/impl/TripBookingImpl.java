eb1a857a4aabe71cde25e6e25af168edc826b67f
Simon Nash <nash@apache.org>
2010-05-14 14:12:35 +0000

    Rename using contribution and launcher to usingsca
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-1.x/trunk@944272 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tutorials/travelsample/contributions/usingsca/src/main/java/com/tuscanyscatours/using/impl/TripBookingImpl.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package com.tuscanyscatours.using.impl;
+
+import java.text.DateFormat;
+import java.util.Date;
+import org.osoa.sca.annotations.Reference;
+
+import com.tuscanyscatours.Bookings;
+import com.tuscanyscatours.Cars;
+import com.tuscanyscatours.Flights;
+import com.tuscanyscatours.Hotels;
+
+public class TripBookingImpl implements Bookings {
+
+    public TripBookingImpl(@Reference(name="cars") Cars cars) {
+        this.cars = cars;
+    }
+    
+    protected Cars cars;
+
+    @Reference
+    protected Flights flights;
+
+    private Hotels hotels;
+
+    @Reference
+    public void setHotels(Hotels hotels) {
+        this.hotels = hotels;
+    }
+
+    public String newBooking(String trip, int people) {
+        /* In a real implementation this method would look up the trip code
+           in the travel catalog to get details of the car, flight and hotel
+           bookings needed.  Instead these details are hard-wired here for
+           test purposes. */   
+        Date startDate = new Date(); //TEMP - need to fix
+        /*
+        try {
+            startDate = DateFormat.getInstance().parse("7 Jul, 2012");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+
+        if (people > 0) {
+            cars.bookCar(startDate, 7, "B");
+            flights.bookFlight("AA123", startDate, people, "Y");
+            hotels.bookHotel("DBH", startDate, 7, "SUP");
+
+            return "HW3546";
+
+        } else {
+            return "NONE";
+        }
+    }
+}
---
+++ tutorials/travelsample/launchers/usingsca/src/main/java/scatours/UsingLauncher.java
@@ -0,0 +1,208 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package scatours;
+
+//import org.apache.activemq.broker.BrokerService;
+import org.apache.tuscany.sca.node.SCAClient;
+import org.apache.tuscany.sca.node.SCANode;
+import org.apache.tuscany.sca.node.SCANodeFactory;
+
+import static scatours.launcher.LauncherUtil.locate;
+
+public class UsingLauncher {
+
+    public static void main(String[] args) throws Exception {
+        runAirportCodes();
+        runBindings();
+        runCarAutowire();
+        runCarPartner();
+        runCarWireElement();
+        runComplexPropertyElement();
+        runComplexPropertyType();
+        runCurrencyConverter();
+        runMultiDomain();
+        runTripAutowire();
+        runTripBooking();
+        runTripWireElement();
+    }
+
+    private static void runAirportCodes() throws Exception {
+        SCANode node =
+            SCANodeFactory.newInstance().createSCANode("test-clients/airportcodes-client.composite",
+                                                       locate("usingsca"));
+
+        node.start();
+
+        Runnable client = ((SCAClient)node).getService(Runnable.class, "AirportCodesClient");
+        client.run();
+
+        node.stop();
+    }
+
+    private static void runBindings() throws Exception {
+        SCANode node =
+            SCANodeFactory.newInstance().createSCANode("test-clients/bookings4-client.composite",
+                                                       locate("usingsca"));
+
+        node.start();
+
+        Runnable client = ((SCAClient)node).getService(Runnable.class, "Bookings4Client");
+        client.run();
+
+        node.stop();
+    }
+
+    private static void runCarAutowire() throws Exception {
+        SCANode node =
+            SCANodeFactory.newInstance().createSCANode("test-clients/carbookings3-client.composite",
+                                                       locate("usingsca"));
+
+        node.start();
+
+        Runnable client = ((SCAClient)node).getService(Runnable.class, "CarBookings3Client");
+        client.run();
+
+        node.stop();
+    }
+
+    private static void runCarPartner() throws Exception {
+        SCANode node =
+            SCANodeFactory.newInstance().createSCANode("test-clients/carbookings1-client.composite",
+                                                       locate("usingsca"));
+
+        node.start();
+
+        Runnable client = ((SCAClient)node).getService(Runnable.class, "CarBookings1Client");
+        client.run();
+
+        node.stop();
+    }
+
+    private static void runCarWireElement() throws Exception {
+        SCANode node =
+            SCANodeFactory.newInstance().createSCANode("test-clients/carbookings2-client.composite",
+                                                       locate("usingsca"));
+
+        node.start();
+
+        Runnable client = ((SCAClient)node).getService(Runnable.class, "CarBookings2Client");
+        client.run();
+
+        node.stop();
+    }
+
+    private static void runComplexPropertyElement() throws Exception {
+        SCANode node =
+            SCANodeFactory.newInstance().createSCANode("test-clients/orders1-client.composite",
+                                                       locate("usingsca"));
+
+        node.start();
+
+        Runnable client = ((SCAClient)node).getService(Runnable.class, "Orders1Client");
+        client.run();
+
+        node.stop();
+    }
+
+    private static void runComplexPropertyType() throws Exception {
+        SCANode node =
+            SCANodeFactory.newInstance().createSCANode("test-clients/orders2-client.composite",
+                                                       locate("usingsca"));
+
+        node.start();
+
+        Runnable client = ((SCAClient)node).getService(Runnable.class, "Orders2Client");
+        client.run();
+
+        node.stop();
+    }
+
+    private static void runCurrencyConverter() throws Exception {
+        SCANode node =
+            SCANodeFactory.newInstance().createSCANode("test-clients/converter-client.composite",
+                                                       locate("usingsca"));
+
+        node.start();
+
+        Runnable client = ((SCAClient)node).getService(Runnable.class, "ConverterClient");
+        client.run();
+
+        node.stop();
+    }
+
+    private static void runMultiDomain() throws Exception {
+        SCANode hotelsNode =
+            SCANodeFactory.newInstance().createSCANode("test-clients/hotelsdomain-client.composite",
+                                                       locate("usingsca"));
+        SCANode toursNode =
+            SCANodeFactory.newInstance().createSCANode("test-clients/toursdomain-client.composite",
+                                                       locate("usingsca"));
+
+        hotelsNode.start();
+        toursNode.start();
+
+        Runnable hotelsClient = ((SCAClient)hotelsNode).getService(Runnable.class, "HotelsDomainClient");
+        hotelsClient.run();
+        Runnable toursClient = ((SCAClient)toursNode).getService(Runnable.class, "ToursDomainClient");
+        toursClient.run();
+
+        toursNode.stop();
+        hotelsNode.stop();
+    }
+
+    private static void runTripAutowire() throws Exception {
+        SCANode node =
+            SCANodeFactory.newInstance().createSCANode("test-clients/bookings3-client.composite",
+                                                       locate("usingsca"));
+
+        node.start();
+
+        Runnable client = ((SCAClient)node).getService(Runnable.class, "Bookings3Client");
+        client.run();
+
+        node.stop();
+    }
+
+    private static void runTripBooking() throws Exception {
+        SCANode node =
+            SCANodeFactory.newInstance().createSCANode("test-clients/bookings1-client.composite",
+                                                       locate("usingsca"));
+
+        node.start();
+
+        Runnable client = ((SCAClient)node).getService(Runnable.class, "Bookings1Client");
+        client.run();
+
+        node.stop();
+    }
+
+    private static void runTripWireElement() throws Exception {
+        SCANode node =
+            SCANodeFactory.newInstance().createSCANode("test-clients/bookings2-client.composite",
+                                                       locate("usingsca"));
+
+        node.start();
+
+        Runnable client = ((SCAClient)node).getService(Runnable.class, "Bookings2Client");
+        client.run();
+
+        node.stop();
+    }
+}
