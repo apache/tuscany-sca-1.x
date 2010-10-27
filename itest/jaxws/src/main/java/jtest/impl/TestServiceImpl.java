9874e4fde243b40035d77d7de4b9d009ca9ae643
Simon Nash <nash@apache.org>
2010-10-27 21:26:27 +0000

    Merge remaining r743192 changes from 2.x to 1.x and add a new itest for @XmlJavaTypeAdapter
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-1.x/trunk@1028105 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/jaxws/src/main/java/jtest/impl/TestServiceImpl.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package jtest.impl;
+
+import org.osoa.sca.annotations.Service;
+
+import jtest.TestAbstract;
+import jtest.TestService;
+
+/**
+ * The test service implementation
+ */
+@Service(TestService.class)
+public class TestServiceImpl implements TestService {
+
+    public void sendAbstract(TestAbstract data1, TestAbstract data2) {
+        System.out.println("data1 is instance of class " + data1.getClass().getName());
+        System.out.println("data2 is instance of class " + data2.getClass().getName());
+        System.out.println(data1.getGreeting() + " " + data2.getGreeting());
+    }
+}
